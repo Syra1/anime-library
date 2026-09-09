1875b1d497cde392987fc4abb398e826b1239c87
syra1 <antoine.fautrel92@gmail.com>
2026-09-09 03:21:53 +0200

    checkbox

--- static/js/media_detail.js
+++ static/js/media_detail.js
@@ -1,7 +1,41 @@
-const saisons = document.querySelectorAll(".season-item-media-detail input[type='checkbox']");
+const saisons = document.querySelectorAll(
+    ".season-item-media-detail input[type='checkbox']"
+);
 
-saisons.forEach((saison) => {
+saisons.forEach((saison, index) => {
     saison.addEventListener("change", () => {
+
+        if (saison.checked) {
+            // Coche toutes les saisons précédentes.
+            for (let i = 0; i < index; i++) {
+                if (!saisons[i].checked) {
+                    saisons[i].checked = true;
+
+                    modifierSaisonVue(
+                        saisons[i].dataset.typeMedia,
+                        saisons[i].dataset.mediaId,
+                        saisons[i].dataset.saisonId,
+                        true
+                    );
+                }
+            }
+        } else {
+            // Décoche toutes les saisons suivantes.
+            for (let i = index + 1; i < saisons.length; i++) {
+                if (saisons[i].checked) {
+                    saisons[i].checked = false;
+
+                    modifierSaisonVue(
+                        saisons[i].dataset.typeMedia,
+                        saisons[i].dataset.mediaId,
+                        saisons[i].dataset.saisonId,
+                        false
+                    );
+                }
+            }
+        }
+
+        // Enregistre la saison actuellement cliquée.
         modifierSaisonVue(
             saison.dataset.typeMedia,
             saison.dataset.mediaId,
@@ -11,7 +45,13 @@ saisons.forEach((saison) => {
     });
 });
 
-async function modifierSaisonVue(typeMedia, mediaId, saisonId, vu) {
+
+async function modifierSaisonVue(
+    typeMedia,
+    mediaId,
+    saisonId,
+    vu
+) {
     await fetch(
         `/${typeMedia}/${mediaId}/saison/${saisonId}/vu?vu=${vu}`,
         {
